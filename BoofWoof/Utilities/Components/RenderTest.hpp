1220fbf54c96245e98882ad5ef62e797f2ab7595
Rachelle GOH <rachelle.goh@digipen.edu>
2024-09-23 02:32:34 +0800

    remove extra include files in RenderTest.hpp

--- BoofWoof/Utilities/Components/RenderTest.hpp
+++ BoofWoof/Utilities/Components/RenderTest.hpp
@@ -1,9 +1,5 @@
 #pragma once
 #include "../Core/ECS/Coordinator.hpp"
-#include <glm/glm.hpp>
-#include <GL/glew.h>
-#include <string>
-#include "../Core/Graphics/Model.h"
 #include "../Core/Graphics/Object.h"
 
 
