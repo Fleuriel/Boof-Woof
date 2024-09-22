47f193fb6c915a97e2294e64a2950bbdb316cb4c
MNGChen <109013440+MNGChen@users.noreply.github.com>
2024-09-22 15:40:11 +0800

    Revert "save.."
    This reverts commit f6f956d0c83163659bfd84c4e9fc4241cf50f009.

--- BoofWoof/Utilities/Components/GraphicsComponent.h
+++ BoofWoof/Utilities/Components/GraphicsComponent.h
@@ -3,53 +3,12 @@
 #include <glm/glm.hpp>
 #include <GL/glew.h>
 #include <string>
-#include "../Core/Graphics/Model.h"
-#include "../Core/Graphics/Shader.h"
+//#include "Object.h"
+
 
 
 class GraphicsComponent {
 public:
-	glm::vec3 position;
-	glm::vec3 scale;
-	glm::vec3 rotation;
-	glm::vec3 color;
-
-
-	Model* model;
-	OpenGLShader* shaderpgm;
-
-
-	glm::mat4 worldMatrix;
-	glm::mat4 getWorldMatrix() {
-
-
-		worldMatrix = glm::mat4(1.0f);
-
-
-
-		worldMatrix = glm::rotate(worldMatrix, rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
-		worldMatrix = glm::rotate(worldMatrix, rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
-		worldMatrix = glm::rotate(worldMatrix, rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
-
-		glm::mat4 scaleMat = glm::mat4(1.0f);
-		scaleMat = glm::scale(scaleMat, scale);
-
-		glm::mat4 translateMat = glm::mat4(1.0f);
-
-		translateMat = glm::translate(translateMat, position);
-
-		worldMatrix = translateMat * worldMatrix * scaleMat;
-
-		/*worldMatrix = glm::mat4{
-			worldMatrix[0][0], worldMatrix[1][0], worldMatrix[2][0], 0.0f,
-			worldMatrix[0][1], worldMatrix[1][1], worldMatrix[2][1], 0.0f,
-			worldMatrix[0][2], worldMatrix[1][2], worldMatrix[2][2], 0.0f,
-			position[0], position[1], position[2], 1.0f
-
-		};*/
-
-		return worldMatrix;
-	}
     // Constructor and destructor
     GraphicsComponent();
 //    GraphicsComponent(const OpenGLModel& model, int id = 0);
