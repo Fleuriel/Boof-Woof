317fb8d6534ffb3070ea57c1eae7bd2500d99987
Sawkon <aaronjunxiang.chan@digipen.edu>
2024-10-06 13:17:57 +0800

    update

--- BoofWoof/Core/AssetManager/Compressonator.cpp
+++ BoofWoof/Core/AssetManager/Compressonator.cpp
@@ -1,3 +1,16 @@
+/**************************************************************************
+ * @file Compressonator.cpp
+ * @author 	Aaron Chan Jun Xiang
+ * @param DP email: aaronjunxiang.chan@digipen.edu [2200880]
+ * @param Course: CS 3401
+ * @param Course: Game Project 3
+ * @date  10/06/2024 (06 OCTOBER 2024)
+ * @brief
+ *
+ * This file defines functions that uses compressonator
+ *
+ *************************************************************************/
+
 #include "Compressonator.h"
 #include <iostream>
 #include <fstream>
