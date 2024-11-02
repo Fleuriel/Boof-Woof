b8a1100ebd68b5dc04d6391ffd3fa8cc0b30c51c
RayShellSs <ritoverycrazyy@gmail.com>
2024-11-02 14:01:18 +0800

    moving player

--- Scripts/Player.hpp
+++ Scripts/Player.hpp
@@ -6,12 +6,27 @@ struct Player final : public Behaviour
 
 	virtual void Update(Entity entity) override
 	{	
-		/*
+		
 		if (m_Engine.getInputSystem().isButtonPressed(GLFW_KEY_W))
 		{
 			m_Engine.SetPosition(entity, m_Engine.GetPosition(entity) + glm::vec3(0.0f, 0.0f, -0.1f));
 		}
-		*/
+
+		if (m_Engine.getInputSystem().isButtonPressed(GLFW_KEY_A))
+		{
+			m_Engine.SetPosition(entity, m_Engine.GetPosition(entity) + glm::vec3(-0.1f, 0.0f, 0.0f));
+		}
+
+		if (m_Engine.getInputSystem().isButtonPressed(GLFW_KEY_S))
+		{
+			m_Engine.SetPosition(entity, m_Engine.GetPosition(entity) + glm::vec3(0.0f, 0.0f, 0.1f));
+		}
+
+		if (m_Engine.getInputSystem().isButtonPressed(GLFW_KEY_D))
+		{
+			m_Engine.SetPosition(entity, m_Engine.GetPosition(entity) + glm::vec3(0.1f, 0.0f, 0.0f));
+		}
+		
 	}
 
 	virtual const char* getBehaviourName() override
