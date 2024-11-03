121f2c4e448ec2e0e28ef3b79750ec9b3afe691b
MNGChen <g.chen@digipen.edu>
2024-11-03 17:37:56 +0800

    updated camera rotate with key C

--- EditorPaws/ImGuiEditor.cpp
+++ EditorPaws/ImGuiEditor.cpp
@@ -822,11 +822,11 @@ void ImGuiEditor::Settings()
 	/// camera control
 	if (g_Input.GetKeyState(GLFW_KEY_UP))
 	{
-		g_Coordinator.GetSystem<GraphicsSystem>()->GetCamera().ProcessKeyboard(Camera_Movement::UP, static_cast<float>(g_Core->m_DeltaTime));
+		g_Coordinator.GetSystem<GraphicsSystem>()->GetCamera().ProcessKeyboard(Camera_Movement::FORWARD, static_cast<float>(g_Core->m_DeltaTime));
 	}
 	if (g_Input.GetKeyState(GLFW_KEY_DOWN))
 	{
-		g_Coordinator.GetSystem<GraphicsSystem>()->GetCamera().ProcessKeyboard(Camera_Movement::DOWN, static_cast<float>(g_Core->m_DeltaTime));
+		g_Coordinator.GetSystem<GraphicsSystem>()->GetCamera().ProcessKeyboard(Camera_Movement::BACKWARD, static_cast<float>(g_Core->m_DeltaTime));
 	}
 	if (g_Input.GetKeyState(GLFW_KEY_LEFT))
 	{
@@ -836,14 +836,28 @@ void ImGuiEditor::Settings()
 	{
 		g_Coordinator.GetSystem<GraphicsSystem>()->GetCamera().ProcessKeyboard(Camera_Movement::RIGHT, static_cast<float>(g_Core->m_DeltaTime));
 	}
-	if (g_Input.GetKeyState(GLFW_KEY_RIGHT_SHIFT))
+	/*if (g_Input.GetKeyState(GLFW_KEY_RIGHT_SHIFT))
 	{
 		g_Coordinator.GetSystem<GraphicsSystem>()->GetCamera().ProcessKeyboard(Camera_Movement::FORWARD, static_cast<float>(g_Core->m_DeltaTime));
 	}
 	if (g_Input.GetKeyState(GLFW_KEY_RIGHT_CONTROL))
 	{
 		g_Coordinator.GetSystem<GraphicsSystem>()->GetCamera().ProcessKeyboard(Camera_Movement::BACKWARD, static_cast<float>(g_Core->m_DeltaTime));
+	}*/
+
+
+	static glm::vec2 lastMousePos = glm::vec2(0.0f);
+	static bool cameraRotated = false;
+	if (g_Input.GetKeyState(GLFW_KEY_C)) 
+	{ 
+		/*cameraRotated = !cameraRotated; 
+		std::cout << " cameraRotated: " << cameraRotated << "\n";
 	}
+	if (cameraRotated){*/
+		glm::vec2 offset = g_Input.GetMousePosition() - lastMousePos;
+		g_Coordinator.GetSystem<GraphicsSystem>()->GetCamera().ProcessMouseMovement(static_cast<float>(offset.x), static_cast<float>(-offset.y),1);
+	}
+	lastMousePos = g_Input.GetMousePosition();
 }
 
 void ImGuiEditor::Scenes()
