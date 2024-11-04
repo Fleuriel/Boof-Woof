dd8c2fe877dbbab6151a1af859276b68d9b0b8ba
Aloysius Ang <aloyang123@gmail.com>
2024-11-04 20:50:07 +0800

    commented out some debug

--- Scripts/Player.hpp
+++ Scripts/Player.hpp
@@ -10,25 +10,25 @@ struct Player final : public Behaviour
 
 		if (m_Engine.getInputSystem().isButtonPressed(GLFW_KEY_W))
 		{
-			std::cout << "movingW" << std::endl;
+			//std::cout << "movingW" << std::endl;
 			velocity.z -= 2.f;
 		}
 
 		if (m_Engine.getInputSystem().isButtonPressed(GLFW_KEY_A))
 		{
-			std::cout << "movingA" << std::endl;
+			//std::cout << "movingA" << std::endl;
 			velocity.x -= 2.f;
 		}
 
 		if (m_Engine.getInputSystem().isButtonPressed(GLFW_KEY_S))
 		{
-			std::cout << "movingS" << std::endl;
+			//std::cout << "movingS" << std::endl;
 			velocity.z += 2.f;
 		}
 
 		if (m_Engine.getInputSystem().isButtonPressed(GLFW_KEY_D))
 		{
-			std::cout << "movingD" << std::endl;
+			//std::cout << "movingD" << std::endl;
 			velocity.x += 2.f;
 		}
 
