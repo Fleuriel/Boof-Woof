f738b6d350c88934a34adfe56bdcf21d188dd6a7
MNGChen <g.chen@digipen.edu>
2024-11-16 19:33:10 +0800

    save

--- BoofWoof/Utilities/Components/ParticleComponent.hpp
+++ BoofWoof/Utilities/Components/ParticleComponent.hpp
@@ -7,12 +7,29 @@
 #include "../Core/Reflection/ReflectionManager.hpp"
 #include "../Core/Reflection/ReflectionManager.hpp"
 
+#define PARTICLE_NUM 100
+
 class ParticleComponent
 {
 public:
-	void Update() {};
 	ParticleComponent() {};
 	~ParticleComponent() {};
+
+	class particle
+	{
+	public:
+		particle() {};
+		~particle() {};
+		
+		glm::vec3 position;
+		glm::vec3 velocity;
+		glm::vec3 direction;
+		float lifeTime;
+		float lifeCount;
+	};
+
+	std::vector<std::unique_ptr<particle>> particles;
+
 
 	REFLECT_COMPONENT(ParticleComponent)
 	{
