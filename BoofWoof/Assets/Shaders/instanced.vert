400c2068afc4ea3011846e21ca3f9887220fec2b
MNGChen <g.chen@digipen.edu>
2024-11-17 00:54:08 +0800

    save

---
+++ BoofWoof/Assets/Shaders/instanced.vert
@@ -0,0 +1,68 @@
+/**************************************************************************
+ * @file Shader.vert
+ * @author 	TAN Angus Yit Hoe
+
+ * @brief
+ *
+ * This file contains the fragment shader
+ *
+ *************************************************************************/
+
+
+#version 450 core
+
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 vertexTransform;
+uniform vec3 objectColor;
+
+layout(location = 0) in vec3 modelPosition;
+layout(location = 1) in vec3 vertexNormal;
+layout(location = 2) in vec2 aTexCoord;  // Texture coordinate input
+layout(location = 3) in vec3 aTangent;
+layout(location = 4) in vec3 aBitangent;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+out layout(location = 0) vec3 vertColor;
+out layout(location = 2) vec3 FragPos;
+out layout(location = 1) vec3 vertNormal; 
+out layout(location = 3) vec2 TexCoord; 
+
+out VS_OUT {
+    vec3 FragPos;
+    vec2 TexCoords;
+    vec3 TangentLightPos;
+    vec3 TangentViewPos;
+    vec3 TangentFragPos;
+} vs_out;
+
+void main()
+{
+    gl_Position = projection * view * vertexTransform * vec4(modelPosition, 1.0f);
+
+    vs_out.FragPos = vec3(vertexTransform * vec4(modelPosition, 1.0f));
+    vs_out.TexCoords = aTexCoord;
+
+    mat3 normalMatrix = transpose(inverse(mat3(vertexTransform)));
+
+    vec3 T = normalize(normalMatrix * aTangent);
+    vec3 N = normalize(normalMatrix * vertexNormal);
+    T = normalize(T - dot(T, N) * N);
+    vec3 B = cross(N, T);
+    mat3 TBN = transpose(mat3(T, B, N));
+
+    vs_out.TangentLightPos = TBN * lightPos;
+    vs_out.TangentViewPos = TBN * viewPos;
+    vs_out.TangentFragPos = TBN * vs_out.FragPos;
+
+   
+
+    vertColor = objectColor;
+    
+    vertNormal = normalMatrix * vertexNormal;
+    TexCoord = aTexCoord;
+    FragPos = vec3(vertexTransform * vec4(modelPosition, 1.0f));
+
+}
--- BoofWoof/Utilities/Components/ParticleComponent.hpp
+++ BoofWoof/Utilities/Components/ParticleComponent.hpp
@@ -20,6 +20,12 @@ public:
 	public:
 		particle() {};
 		~particle() {};
+
+		void update(float dt)
+		{
+			position += velocity * dt;
+			lifeCount += dt;
+		}
 		
 		glm::vec3 position;
 		glm::vec3 velocity;
@@ -28,7 +34,73 @@ public:
 		float lifeCount;
 	};
 
-	std::vector<std::unique_ptr<particle>> particles;
+	void init( )
+	{
+		for (int i = 0; i < PARTICLE_NUM; i++)
+		{
+			translation[i] = glm::vec3(0.0f, 0.0f, 0.0f);
+			auto ptc = std::make_unique<particle>();
+			ptc->position = glm::vec3(0.01f, 0.0f, 0.0f);
+			ptc->velocity = glm::vec3(0.0f, 0.0f, 0.0f);
+			ptc->direction = glm::vec3(0.0f, 0.0f, 0.0f);
+			ptc->lifeTime = 0.0f;
+			ptc->lifeCount = 0.0f;
+			particles.push_back(std::move(ptc));
+		}
+
+		glGenBuffers(1, &instanceVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+		glBufferData(GL_ARRAY_BUFFER, PARTICLE_NUM * sizeof(glm::vec3), &translation[0], GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+		glGenVertexArrays(1, &quadVAO);
+		glGenBuffers(1, &quadVBO);
+		glBindVertexArray(quadVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(mesh.vertices), &mesh.vertices, GL_STATIC_DRAW);
+		// vertex attributes
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+
+		// vertex normals
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+
+		// vertex texture coords
+		glEnableVertexAttribArray(3);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+
+
+
+		// instance VBO
+		glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glVertexAttribDivisor(2, 1);
+		glBindVertexArray(0);
+
+
+	}
+
+	void update(float dt)
+	{
+		for (int i = 0; i < PARTICLE_NUM; i++)
+		{
+			particles[i]->update(dt);
+		}
+	}
+
+	void draw(OpenGLShader shader)
+	{
+		shader.Use();
+		glBindVertexArray(quadVAO);
+		glDrawArraysInstanced(GL_TRIANGLES, 0, 6, PARTICLE_NUM);
+		glBindVertexArray(0);
+		shader.UnUse();
+	}
+	
 
 
 	REFLECT_COMPONENT(ParticleComponent)
@@ -38,7 +110,12 @@ public:
 
 
 private:
+	std::vector<std::unique_ptr<particle>> particles;
+	glm::vec3 translation[PARTICLE_NUM]{};
+	GLuint instanceVBO{}, quadVAO{}, quadVBO{};
+	Mesh mesh;
 
+	
 };
 
 
