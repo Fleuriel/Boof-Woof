d4c817977be33cdae980580f4eb2347732d6623a
RayShellSs <ritoverycrazyy@gmail.com>
2024-11-21 22:49:44 +0800

    save

--- Game/Systems/BoneCatcher/BoneCatcher.cpp
+++ Game/Systems/BoneCatcher/BoneCatcher.cpp
@@ -24,10 +24,26 @@ void BoneCatcher::OnInitialize()
 
 				if (g_Coordinator.HaveComponent<TransformComponent>(m_DogHead))
 				{
-					initialDogPos = g_Coordinator.GetComponent<TransformComponent>(m_DogHead).GetPosition();
+					DogPos = g_Coordinator.GetComponent<TransformComponent>(m_DogHead).GetPosition();
+					DogScale = g_Coordinator.GetComponent<TransformComponent>(m_DogHead).GetScale();
+					initialDogPos = DogPos;
 				}
+			}
 
-				break;
+			if (g_Coordinator.GetComponent<MetadataComponent>(entity).GetName() == "CatchZone")
+			{
+				m_CatchZone = entity;
+
+				if (g_Coordinator.HaveComponent<TransformComponent>(m_CatchZone))
+				{
+					CatchZonePos = g_Coordinator.GetComponent<TransformComponent>(m_CatchZone).GetPosition();
+					CatchZoneScale = g_Coordinator.GetComponent<TransformComponent>(m_CatchZone).GetScale();
+
+					BoxMin = CatchZonePos - CatchZoneScale * 0.5f;	// Bottom left
+					BoxMax = CatchZonePos + CatchZoneScale * 0.5f;  // Top right
+
+					TeethPos = { DogPos.x + 0.05f, DogPos.y - (DogScale.y / 2) + (TeethScale.y / 2), 0.f };
+				}
 			}
 		}
 	}
@@ -37,25 +53,26 @@ void BoneCatcher::OnUpdate(double deltaTime)
 {
 	if (m_IsMoving)
 	{
-		// Update position based on direction and speed
-		initialDogPos.x += m_Direction * m_Speed * static_cast<float>(deltaTime);
+		MoveLeftRightVisual(deltaTime);
+	}
+	else 
+	{
+		BiteDownVisual(deltaTime);
+	}
 
-		if (g_Coordinator.HaveComponent<TransformComponent>(m_DogHead))
-		{
-			g_Coordinator.GetComponent<TransformComponent>(m_DogHead).SetPosition(initialDogPos);
-		}
+	bool isInRange = (TeethPos.x >= BoxMin.x && TeethPos.x <= BoxMax.x) && (TeethPos.y >= BoxMin.y && TeethPos.y <= BoxMax.y);
 
-		// Check boundaries and reverse direction if necessary
-		if (initialDogPos.x >= m_MaxPos)
-		{
-			initialDogPos.x = m_MaxPos;
-			m_Direction = -1; // Move left
-		}
-		if (initialDogPos.x <= m_MinPos)
-		{
-			initialDogPos.x = m_MinPos;
-			m_Direction = 1; // Move right
-		}
+	if (isInRange)
+	{
+		std::cout << "Hit detected!" << std::endl;
+		// Play YAY sound
+		// pass, change catchzone to smaller & speed faster.
+	}
+	else
+	{
+		std::cout << "No hit." << std::endl;
+		// Play BOO sound
+		// Fail, same speed, same catchzone.
 	}
 
 	if (m_ShouldDestroy)
@@ -72,11 +89,75 @@ void BoneCatcher::OnUpdate(double deltaTime)
 
 void BoneCatcher::Stop()
 {
+	// Stop then visual feedback up down
 	m_IsMoving = false;
-	m_DestroyTimer = 2.0f;
-	m_ShouldDestroy = true;
+	
+
+	//m_DestroyTimer = 2.0f;
+	//m_ShouldDestroy = true;
 }
 
+void BoneCatcher::MoveLeftRightVisual(double deltaTime)
+{
+	// Update position based on direction and speed
+	DogPos.x += m_Direction * m_Speed * static_cast<float>(deltaTime);
+	TeethPos = { DogPos.x + 0.05f, DogPos.y - (DogScale.y / 2) + (TeethScale.y / 2), 0.f };
+
+	if (g_Coordinator.HaveComponent<TransformComponent>(m_DogHead))
+	{
+		g_Coordinator.GetComponent<TransformComponent>(m_DogHead).SetPosition(DogPos);
+	}
+
+	// Check boundaries and reverse direction if necessary
+	if (DogPos.x >= m_MaxPos)
+	{
+		DogPos.x = m_MaxPos;
+		m_Direction = -1; // Move left
+	}
+	if (DogPos.x <= m_MinPos)
+	{
+		DogPos.x = m_MinPos;
+		m_Direction = 1; // Move right
+	}
+}
+
+void BoneCatcher::BiteDownVisual(double deltaTime)
+{
+	if (!m_Down)
+	{
+		DogPos.y -= 0.2f;
+
+		if (g_Coordinator.HaveComponent<TransformComponent>(m_DogHead))
+		{
+			g_Coordinator.GetComponent<TransformComponent>(m_DogHead).SetPosition(DogPos);
+		}
+
+		m_Down = true;  // prevent moving down again until reset.
+
+		// 2 seconds to wait before moving back up
+		m_DownTimer = 2.0f;
+	}
+
+	// Decrease the timer and move head back up after 2 seconds
+	if (m_Down && m_DownTimer > 0.0f)
+	{
+		m_DownTimer -= static_cast<float>(deltaTime);
+
+		if (m_DownTimer <= 0.0f)
+		{
+			// Move the dog head back up
+			DogPos.y += 0.2f;
+
+			if (g_Coordinator.HaveComponent<TransformComponent>(m_DogHead))
+			{
+				g_Coordinator.GetComponent<TransformComponent>(m_DogHead).SetPosition(DogPos);
+			}
+
+			m_Down = false;
+			m_IsMoving = true;
+		}
+	}
+}
 void BoneCatcher::ClearBoneCatcher()
 {
 	// Just remove whatever we had stored from the current alive entity and destroy them
--- Game/Systems/BoneCatcher/BoneCatcher.h
+++ Game/Systems/BoneCatcher/BoneCatcher.h
@@ -9,15 +9,28 @@ public:
 	void OnUpdate(double deltaTime);
 	void Stop();
 
+	void MoveLeftRightVisual(double deltaTime);
+	void BiteDownVisual(double deltaTime);
 	void ClearBoneCatcher();
 
 private:
-	Entity m_DogHead{};
-	glm::vec3 initialDogPos{};
-	float m_Speed{ 2.5f }, m_DestroyTimer{ 0.f };
-	bool m_IsMoving{ true }, m_ShouldDestroy{ false };
+	Entity m_DogHead{}, m_CatchZone{};
+
+	// Dog
+	glm::vec3 initialDogPos{}, DogPos{}, DogScale{};
+	float m_Speed{ 2.5f }, m_DestroyTimer{ 0.f }, m_DownTimer{};
+	bool m_IsMoving{ true }, m_ShouldDestroy{ false }, m_Down{ false }, m_Up{ false };
 	int m_Direction{ 1 }; // 1 for right, -1 for left
 	float m_MinPos{ -1.9f }, m_MaxPos{ 2.2f };
+
+	// CatchZone
+	glm::vec3 CatchZonePos{};
+	glm::vec3 CatchZoneScale{};
+	glm::vec3 BoxMin{}, BoxMax{};
+
+	// Dog Teeth
+	glm::vec3 TeethScale{ 0.5f, 0.2f, 0.f };
+	glm::vec3 TeethPos{};
 
 protected:
 
