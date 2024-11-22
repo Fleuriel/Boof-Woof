459c0a4171ee78a523a05608dbdd56a534e8ac2a
MNGChen <g.chen@digipen.edu>
2024-11-23 03:25:16 +0800

    1st person camera rotate

--- Game/Systems/CameraController/CameraController.cpp
+++ Game/Systems/CameraController/CameraController.cpp
@@ -17,6 +17,11 @@ void CameraController::Update(float deltaTime)
     {
         UpdateThirdPersonView(camera);
     }
+
+	/*if (currentMode == CameraMode::SHIFTING)
+	{
+		UpdateShiftingView(camera);
+	}*/
 }
 
 void CameraController::ToggleCameraMode()
@@ -24,26 +29,36 @@ void CameraController::ToggleCameraMode()
     if (currentMode == CameraMode::FIRST_PERSON)
     {
         currentMode = CameraMode::THIRD_PERSON;
+		//lastMode = CameraMode::FIRST_PERSON;
     }
     else if (currentMode == CameraMode::THIRD_PERSON)
     {
         currentMode = CameraMode::FIRST_PERSON;
+		//lastMode = CameraMode::THIRD_PERSON;
     }
 }
 
 void CameraController::UpdateFirstPersonView(CameraComponent& camera)
 {
     // Match the camera's position to the player's position
-    if (g_Coordinator.HaveComponent<TransformComponent>(playerEntity))
+    if (!g_Coordinator.HaveComponent<TransformComponent>(playerEntity))
     {
-        glm::vec3 playerPos = g_Coordinator.GetComponent<TransformComponent>(playerEntity).GetPosition();
-
-        // eye offset 
-        glm::vec3 eyeOffset = glm::vec3(0.0f, 0.193f, -1.189f);
-
-        // Set the camera's position to the player's eye position
-        camera.Position = playerPos + eyeOffset;
+		return;
     }
+
+    glm::vec3 playerPos = g_Coordinator.GetComponent<TransformComponent>(playerEntity).GetPosition();
+
+    // eye offset 
+    glm::vec3 eyeOffset = glm::vec3(0.0f, 0.193f, -1.189f);
+    // calculate the rotation matrix
+    glm::mat4 rotationMatrix = glm::mat4(1.0f);
+    rotationMatrix = glm::rotate(rotationMatrix, glm::radians(camera.Yaw + 90), glm::vec3(0.0f, -1.0f, 0.0f));
+    //rotationMatrix = glm::rotate(rotationMatrix, glm::radians(camera.Pitch), glm::vec3(-1.0f, 0.0f, 0.0f));
+
+    // calculate the new offset
+    glm::vec3 newOffset = glm::vec3(rotationMatrix * glm::vec4(eyeOffset, 1.0f));
+
+	camera.Position = playerPos + newOffset;
 
     // Handle mouse input for camera rotation
     static glm::vec2 lastMousePos = glm::vec2(g_Input.GetMousePosition().x, g_Input.GetMousePosition().y);
@@ -72,6 +87,14 @@ void CameraController::UpdateFirstPersonView(CameraComponent& camera)
     // Ensure the camera's direction is updated based on its yaw and pitch
     camera.updateCameraVectors();
 
+    //calculate the translation rotation x, y and z
+    float x = g_Coordinator.GetComponent<TransformComponent>(playerEntity).GetRotation().x;
+    float y = glm::radians(-camera.Yaw);
+    float z = g_Coordinator.GetComponent<TransformComponent>(playerEntity).GetRotation().z;
+
+
+    glm::vec3 newrotation = glm::vec3(x, y, z);
+    g_Coordinator.GetComponent<TransformComponent>(playerEntity).SetRotation(newrotation);
 
 }
 
@@ -134,4 +157,12 @@ void CameraController::UpdateThirdPersonView(CameraComponent& camera)
 
     glm::vec3 newrotation = glm::vec3(x, y, z);
     g_Coordinator.GetComponent<TransformComponent>(playerEntity).SetRotation(newrotation);
+}
+
+CameraMove CameraController::getfirstPersonCameraMove()
+{
+	CameraMove cm;
+
+	return cm;
+
 }
--- Game/Systems/CameraController/CameraController.h
+++ Game/Systems/CameraController/CameraController.h
@@ -6,23 +6,40 @@
 enum class CameraMode
 {
     FIRST_PERSON,
-    THIRD_PERSON
+    THIRD_PERSON,
+    SHIFTING
+};
+
+struct CameraMove {
+    glm::vec3 positionMove = glm::vec3(0.0f);
+    float pitchMove = 0.0f;
+    float yawMove = 0.0f;
 };
 
 class CameraController {
 public:
-    CameraController(Entity playerEntity) : playerEntity(playerEntity) { }
+    
+    CameraController(Entity playerEntity) : playerEntity(playerEntity), cameraMove{} { }
 
     void Update(float deltaTime);
 
     void ToggleCameraMode();
 
+	//void update_cameramove(float time);
+
 private:
     void UpdateFirstPersonView(CameraComponent& camera);
     void UpdateThirdPersonView(CameraComponent& camera);
+	void UpdateShiftingView(CameraComponent& camera);
+
+    CameraMove getfirstPersonCameraMove();
+    CameraMove getThirdPersonCameraMove();
+
 
 private:
     Entity playerEntity{};
     CameraMode currentMode = CameraMode::FIRST_PERSON;
     glm::vec3 thirdPersonOffset = glm::vec3(0.0f, 2.0f, -5.0f); // Example offset for third-person view
+	CameraMode lastMode = CameraMode::FIRST_PERSON;
+    CameraMove cameraMove;
 };
