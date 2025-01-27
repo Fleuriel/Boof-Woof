9a467ac31e07dd465b0fdf841be100d3d911f6d4
Wolfslayer03 <60912661+Wolfslayer03@users.noreply.github.com>
2025-01-28 01:00:20 +0800

    Treat Script completed

--- BoofWoof/Core/Logic/Script_to_Engine.h
+++ BoofWoof/Core/Logic/Script_to_Engine.h
@@ -15,7 +15,7 @@
 #pragma warning(disable: 6385 6386)
 #include <Jolt/Physics/Body/Body.h>
 
-class Script_to_Engine : public engine_interface, public input_interface, public audio_interface
+class Script_to_Engine : public engine_interface, public input_interface, public audio_interface, public physics_interface
 {
 public:
 
@@ -51,10 +51,30 @@ public:
 		 g_Audio.PlayFile(pSoundName);
 	}
 
+	virtual void PlaySoundById(const char* soundId) override
+	{
+		std::string fullPath = std::string(FILEPATH_ASSET_AUDIO) + "/" + soundId;
+		g_Audio.PlayFile(fullPath.c_str());
+	}
+
+	// END OF AUDIO INTERFACE
+
+	// PHYSICS INTERFACE
+	virtual physics_interface& getPhysicsSystem() override
+	{
+		return *this;
+	}
+
+	virtual void RemoveBody(Entity entity) override
+	{
+		g_Coordinator.GetSystem<MyPhysicsSystem>()->RemoveEntityBody(entity);
+	}
+
 	// ENGINE INTERFACE
 
 	virtual void DestroyEntity(Entity entity) override
 	{
+		
 		g_Coordinator.DestroyEntity(entity);
 	}
 	
@@ -187,12 +207,6 @@ public:
 	virtual glm::vec3 GetCameraUp(Entity entity) override
 	{
 		return g_Coordinator.GetComponent<CameraComponent>(entity).GetCameraUp();
-	}
-
-	virtual void PlaySoundById(const char* soundId) override
-	{
-		std::string fullPath = std::string(FILEPATH_ASSET_AUDIO) + "/" + soundId;
-		g_Audio.PlayFile(fullPath.c_str());
 	}
 
 
--- ScriptWoof/Treat.hpp
+++ ScriptWoof/Treat.hpp
@@ -11,7 +11,6 @@ struct Treat final : public Behaviour
 
 	virtual void Update(Entity entity) override
 	{
-		std::cout << "Treat Update" << std::endl;
 		//Check if player is colliding with treat
 		if (m_Engine.IsColliding(entity))
 		{
@@ -19,15 +18,18 @@ struct Treat final : public Behaviour
 			//Get the name of the entity that is colliding with the treat
 			const char* collidingEntityName = m_Engine.GetCollidingEntityName(entity);
 
-			std::cout << "Scripted Entity is colliding with" << collidingEntityName << std::endl;
+			std::cout << "Scripted Entity is colliding with " << collidingEntityName << std::endl;
 
 			//Check if the entity is the player
-			if (strcmp(collidingEntityName, "Player") == 1)
+			if (std::strcmp(collidingEntityName, "Player") == 0) // If the entity is the player, destroy the treat
 			{
+				std::cout << "Player is colliding with treat" << std::endl;
+
 				//Play sound
 				m_Engine.getAudioSystem().PlaySound("TreatSound");
 
 				//Destroy the treat
+				m_Engine.getPhysicsSystem().RemoveBody(entity);
 				m_Engine.DestroyEntity(entity);
 			}
 		}
@@ -38,5 +40,10 @@ struct Treat final : public Behaviour
 	{
 		UNREFERENCED_PARAMETER(entity);
 		//std::cout << "Treat Destroy" << std::endl;
+	}
+
+	virtual const char* getBehaviourName() override
+	{
+		return "Treat";
 	}
 };
