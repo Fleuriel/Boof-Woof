7700d4e8bbf4ec1cb8a7d360feff13d3972fd06e
RayShellSs <ritoverycrazyy@gmail.com>
2025-02-02 16:39:33 +0800

    Checklist to Puppies!

--- PawsieAdventure/GameMain.cpp
+++ PawsieAdventure/GameMain.cpp
@@ -28,9 +28,9 @@ void InitializeLevels()
     g_LevelManager.RegisterLevel("LoadingLevel", new LoadingLevel());
 
     // Set the initial level
-    g_LevelManager.Initialize("Splashscreen");
-    g_LevelManager.SetNextLevel("Splashscreen");
-    g_LevelManager.SetPreviousLevel("Splashscreen");
+    g_LevelManager.Initialize("MainHall");
+    g_LevelManager.SetNextLevel("MainHall");
+    g_LevelManager.SetPreviousLevel("MainHall");
 }
 
 int RunGame()
