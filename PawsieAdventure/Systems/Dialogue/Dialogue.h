2d9a71e66bf638c97c42d5daf019ee74da234680
RayShellSs <ritoverycrazyy@gmail.com>
2025-02-23 06:41:53 +0800

    save dialogue setup

---
+++ PawsieAdventure/Systems/Dialogue/Dialogue.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <EngineCore.h>
+
+enum class DialogueState {
+	DEFAULT,
+	TUTORIALSTART,
+	TUTORIALEND,
+	TOUCHBALL,
+	TOUCHBONE,
+	DONTWASTETIME,
+	OUTOFLIBRARY,
+	REXSAWYOU,
+	SEARCHINGFORPUPS,
+	FOUNDPUP1,
+	TWOMORETOGO,
+	DISGUSTED,
+	SEARCHINGFORPUPS2,
+	FOUNDPUP2,
+	ONEMORETOGO,
+	SEARCHINGFORPUPS3,
+	FOUNDPUP3,
+	ALLPUPSFOUND,
+	STAYCLOSE,
+	REXSAWYOU2,
+	BREAKROPES,
+	BROKEROPE1,
+	FREED,
+};
+
+class Dialogue
+{
+public:
+	void OnInitialize();
+	void OnUpdate(double deltaTime);
+	void OnShutdown();
+	std::string getDialogue();
+	void setDialogue(DialogueState newState, Entity entity);
+	void Reset();
+
+private:
+	Entity m_D1{};
+	DialogueState m_CurrentState{ DialogueState::DEFAULT };
+
+protected:
+	// Storage is for those entity you added in when you load a scene file
+	// during exit, can just delete those entity - clearing spawned entities
+	std::vector<Entity> storage;
+};
