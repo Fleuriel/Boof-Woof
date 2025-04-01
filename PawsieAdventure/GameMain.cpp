04edac3b747a51bf4f3c3188cc8c73e1e8b3da04
Wolfslayer03 <60912661+Wolfslayer03@users.noreply.github.com>
2025-04-02 03:42:33 +0800

    StartingRoom Sniffing done

--- PawsieAdventure/GameMain.cpp
+++ PawsieAdventure/GameMain.cpp
@@ -34,9 +34,9 @@ void InitializeLevels()
 	g_LevelManager.RegisterLevel("TYVM", new TYVM());
 
     // Set the initial level
-    g_LevelManager.Initialize("MainHall");
-    g_LevelManager.SetNextLevel("MainHall");
-    g_LevelManager.SetPreviousLevel("MainHall");
+    g_LevelManager.Initialize("StartingRoom");
+    g_LevelManager.SetNextLevel("StartingRoom");
+    g_LevelManager.SetPreviousLevel("StartingRoom");
 }
 
 int RunGame()
