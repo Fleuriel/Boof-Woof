7144726efa76cc01beeaf16724560fb3abfcd24e
RayShellSs <ritoverycrazyy@gmail.com>
2025-04-04 02:46:48 +0800

    splashscreen

--- PawsieAdventure/GameMain.cpp
+++ PawsieAdventure/GameMain.cpp
@@ -35,9 +35,9 @@ void InitializeLevels()
 
     // Set the initial level
 
-    g_LevelManager.Initialize("TimeRush");
-    g_LevelManager.SetNextLevel("TimeRush");
-    g_LevelManager.SetPreviousLevel("TimeRush");
+    g_LevelManager.Initialize("Splashscreen");
+    g_LevelManager.SetNextLevel("Splashscreen");
+    g_LevelManager.SetPreviousLevel("Splashscreen");
 
 }
 
