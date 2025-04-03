169e18615d31a6180784b2ac71e5f82a9c68e795
tan.a <tan.a@digipen.edu>
2025-04-04 03:10:51 +0800

    Main Up

--- PawsieAdventure/GameMain.cpp
+++ PawsieAdventure/GameMain.cpp
@@ -35,9 +35,9 @@ void InitializeLevels()
 
     // Set the initial level
 
-    g_LevelManager.Initialize("TimeRush");
-    g_LevelManager.SetNextLevel("TimeRush");
-    g_LevelManager.SetPreviousLevel("TimeRush");
+    g_LevelManager.Initialize("MainHall");
+    g_LevelManager.SetNextLevel("MainHall");
+    g_LevelManager.SetPreviousLevel("MainHall");
 
 }
 
